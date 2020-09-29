9e69dbf0413010d17075779c218a74b75d8e8ab8
Hector Hidalgo <marce.753@gmail.com>
2020-09-29 12:37:31 -0400

    Fixed string comparison bug

--- src/main/java/Proyecte/EnterpriseLogic/ReceivableFactory.java
+++ src/main/java/Proyecte/EnterpriseLogic/ReceivableFactory.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class ReceivableFactory {
     public static Receivable getReceivable(String type, HashMap<String, Object> data){
 		  
-	    if (type == "FriendsReceivable")
+	    if (type.equals("FriendsReceivable"))
 	      return new FriendsReceivable(data.get("friends").toString());
 	    
 	    return null;
